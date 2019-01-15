5d16ff7e0f68060cc38845cf0914173f771d60f7
pickuppatterns <pickup.patterns@gmail.com>
2019-01-15 10:34:27 -0800

    adds copy file promise

--- promises.js
+++ promises.js
@@ -1,15 +1,23 @@
 const fs = require('fs').promises;
 
+//READ
 fs.readFile('./.travis.yml', { encoding: 'utf-8' })
   .then(data => console.log(data))
   .catch(err => console.error(err));
 
+//WRITE
 fs.writeFile('test.txt', 'thiswrotetoyourfile', { encoding: 'utf-8' })
-  .then(()=> console.log('DONE!'))
+  .then(()=> console.log('WRITE FILE DONE!'))
   .catch(err => console.error(err));
 
-//Promise States:
+//COPY
+fs.readFile('./.travis.yml', { encoding: 'utf-8' })
+  .then(data => fs.writeFile('./.travis-copy.yml', data, { encoding: 'utf-8' }))
+  .then(()=> console.log('COPY FILE DONE!'))
+  .catch(err => console.error(err));
 
+
+//Promise States:
 //pending state ->waiting for the promise to finish
 //fulfilled -> the promised finished & resolved
 //rejected -> the promise finished & rejected
