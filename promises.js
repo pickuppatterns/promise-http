ccb1cfdc870f06ebb2e4c2ed22cdcff141d2aab0
pickuppatterns <pickup.patterns@gmail.com>
2019-01-15 10:19:56 -0800

    adds write a file promise

--- promises.js
+++ promises.js
@@ -4,6 +4,16 @@ fs.readFile('./.travis.yml', { encoding: 'utf-8' })
   .then(data => console.log(data))
   .catch(err => console.error(err));
 
+fs.writeFile('test.txt', 'thiswrotetoyourfile', { encoding: 'utf-8' })
+  .then(()=> console.log('DONE!'))
+  .catch(err => console.error(err));
+
+//Promise States:
+
+//pending state ->waiting for the promise to finish
+//fulfilled -> the promised finished & resolved
+//rejected -> the promise finished & rejected
+
 
 
 
