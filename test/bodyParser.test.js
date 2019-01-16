026b79a95267a47660f7db7989e4ca672215e47a
pickuppatterns <pickup.patterns@gmail.com>
2019-01-16 09:52:05 -0800

    updates bodyParser test

--- test/bodyParser.test.js
+++ test/bodyParser.test.js
@@ -1,12 +1,12 @@
 const bodyParser = require('../lib/bodyParser');
 const EventEmitter = require('events');
 
-
 describe('bodyParser', () => {
   it('parses a requests body', () => {
     const req = new EventEmitter();
-    req.headers = {};
-    req.getHeader = () => 'application/json';
+    req.headers = {
+      'content-type': 'application/json'
+    };
     req.method = 'POST';
 
     const promise = bodyParser(req)
@@ -14,7 +14,7 @@ describe('bodyParser', () => {
         expect(json).toEqual({ testing: 1234 });
       });
 
-    req.emit('data', JSON.stringify({ testing:1234 }));
+    req.emit('data', JSON.stringify({ testing: 1234 }));
     req.emit('end');
 
     return promise;
