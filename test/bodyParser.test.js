b9d7846624da567662e33e14e7d93690d933f468
pickuppatterns <pickup.patterns@gmail.com>
2019-01-15 16:57:51 -0800

    add body parser

---
+++ test/bodyParser.test.js
@@ -0,0 +1,14 @@
+const bodyParser = require('../lib/bodyPrser');
+
+describe('bodyParser', ()=> {
+  it('parses a requests body', () => {
+const req = new EventEmitter();
+
+const promise = bodyParser(req)
+.then(json => {
+  expect(json).toEqual({ testing: 1234});
+
+req.emit('data', JSON.stringify({ testing:1234 }));
+req.emit('end');
+});
+});
