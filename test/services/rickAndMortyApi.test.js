5d2ac07756891f1420373830dd455e681ab85e27
pickuppatterns <pickup.patterns@gmail.com>
2019-01-16 07:58:19 -0800

    removes .only from it from Jest test assertion

--- test/services/rickAndMortyApi.test.js
+++ test/services/rickAndMortyApi.test.js
@@ -1,7 +1,7 @@
 const { getCharacter } = require('../../lib/services/rickAndMortyApi');
 
 describe('rick & morty service', () => {
-  it.only('gets a character by id', () => {
+  it('gets a character by id', () => {
     return getCharacter(1)
       .then(character => {
         expect(character).toEqual({
