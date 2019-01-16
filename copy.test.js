775faebc4ae87e9e34cbc799c927772982a9b94f
pickuppatterns <pickup.patterns@gmail.com>
2019-01-16 08:11:12 -0800

    updates Promise.all method in file copy test

--- copy.test.js
+++ copy.test.js
@@ -1,15 +1,24 @@
 const copy = require('./copy');
 const fs = require('fs');
-
+const fsPromises = require('fs').promises;
 
 describe('copy', () => {
   afterEach(() => {
     delete('./.travis-copy.yml');
+    return fsPromises.unlink('./.travis-copy.yml');
   });
   it('copies a file', () => {
     return copy('./.travis.yml', './.travis-copy.yml')
       .then(() => {
         expect(fs.readFileSync('./.travis.yml')).toEqual(fs.readFileSync('./.travis-copy.yml'));
+        return Promise.all([
+          fsPromises.readFile('./.travis.yml'),
+          fsPromises.readFile('./.travis-copy.yml')
+        ]);
+      })
+      .then([travisYml, travisCopyYml] => {
+        //expect travisYml to equal travisCopyYml
+        expect(travisYml).toEqual(travisCopyYml);
       })
       .catch(err => expect(err).toBeFalsy());
   });
